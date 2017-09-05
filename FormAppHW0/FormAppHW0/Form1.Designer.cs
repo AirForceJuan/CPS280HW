4ebb9b820b2b82cee0e2d3587274e86e01c87811
AirForceJuan <31664858+AirForceJuan@users.noreply.github.com>
2017-09-05 16:44:52 -0400

    added input validation for the user

--- FormAppHW0/FormAppHW0/Form1.Designer.cs
+++ FormAppHW0/FormAppHW0/Form1.Designer.cs
@@ -28,42 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lnAmount = new System.Windows.Forms.TextBox();
+            this.trmYears = new System.Windows.Forms.TextBox();
+            this.iRate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // lnAmount
             // 
-            this.textBox1.Location = new System.Drawing.Point(148, 48);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.lnAmount.Location = new System.Drawing.Point(148, 48);
+            this.lnAmount.Name = "lnAmount";
+            this.lnAmount.Size = new System.Drawing.Size(100, 20);
+            this.lnAmount.TabIndex = 0;
+            this.lnAmount.TextChanged += new System.EventHandler(this.lnAmount_TextChanged);
             // 
-            // textBox2
+            // trmYears
             // 
-            this.textBox2.Location = new System.Drawing.Point(148, 106);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.trmYears.Location = new System.Drawing.Point(148, 106);
+            this.trmYears.Name = "trmYears";
+            this.trmYears.Size = new System.Drawing.Size(100, 20);
+            this.trmYears.TabIndex = 1;
+            this.trmYears.TextChanged += new System.EventHandler(this.trmYears_TextChanged);
             // 
-            // textBox3
+            // iRate
             // 
-            this.textBox3.Location = new System.Drawing.Point(148, 172);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 2;
+            this.iRate.Location = new System.Drawing.Point(148, 162);
+            this.iRate.Name = "iRate";
+            this.iRate.Size = new System.Drawing.Size(100, 20);
+            this.iRate.TabIndex = 2;
+            this.iRate.TextChanged += new System.EventHandler(this.iRate_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Loan Amount ($)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Loan Term (Years)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Interest Rate (%)";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 231);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(657, 156);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 86);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(254, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Must be a Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(254, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Must be a Year";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(254, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Must be a %";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(681, 399);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iRate);
+            this.Controls.Add(this.trmYears);
+            this.Controls.Add(this.lnAmount);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,9 +165,17 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox lnAmount;
+        private System.Windows.Forms.TextBox trmYears;
+        private System.Windows.Forms.TextBox iRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- FormAppHW0/FormAppHW0/Form1.cs
+++ FormAppHW0/FormAppHW0/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FormAppHW0
+    /// </summary>
+    /// Form App that Calculates the Payoff of a loan
+    /// </summary>
 {
     public partial class Form1 : Form
     {
@@ -16,5 +19,65 @@ namespace FormAppHW0
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+        }
+
+        private void lnAmount_TextChanged(object sender, EventArgs e)
+        {
+            int val;
+            if ((!Int32.TryParse(lnAmount.Text, out val)))
+            {
+                label4.Visible = true;
+            }
+            if ((Int32.TryParse(lnAmount.Text, out val)))
+            {
+                label4.Visible = false;
+            }
+
+           
+        }
+
+        private void trmYears_TextChanged(object sender, EventArgs e)
+        {
+            int val;
+            if ((!Int32.TryParse(trmYears.Text, out val)))
+            {
+                label5.Visible = true;
+            }
+            if ((Int32.TryParse(trmYears.Text, out val)))
+            {
+                label5.Visible = false;
+            }
+            string num = val.ToString();
+            if (num.Length >= 4)
+            {
+                label5.Visible = true;
+            }
+        }
+
+        private void iRate_TextChanged(object sender, EventArgs e)
+        {
+            double val;
+            if ((!double.TryParse(iRate.Text, out val)))
+            {
+                label6.Visible = true;
+            }
+            if ((double.TryParse(iRate.Text, out val)))
+            {
+                label6.Visible = false;
+            }
+
+            string num = val.ToString();
+            if (num.Length >= 5)
+            {
+                label6.Visible = true;
+            }
+        }
+
     }
 }
