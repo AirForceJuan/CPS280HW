7042164780678e82c15ab6e4d0a7e64dfb8c4b8e
AirForceJuan <rojas1nd@cmich.edu>
2017-09-07 08:37:59 -0700

    Final Program. Works and Computers the interest correctly. Formatting is a little off though

--- FormAppHW0/FormAppHW0/Form1.cs
+++ FormAppHW0/FormAppHW0/Form1.cs
@@ -22,14 +22,14 @@ namespace FormAppHW0
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            label4.Visible = false;
-            label5.Visible = false;
-            label6.Visible = false;
+            label4.Visible = false; // sets "must me a number" labal to false
+            label5.Visible = false; // sets "must be a year" labal to false
+            label6.Visible = false; // sets "must be a %" labal to false
 
             
         }
 
-        private void LnAmount_TextChanged(object sender, EventArgs e)
+        private void LnAmount_TextChanged(object sender, EventArgs e) //input validation for loan amount
         {
             int val;
             if ((!Int32.TryParse(loanAmount.Text, out val)))
@@ -43,7 +43,7 @@ namespace FormAppHW0
            
         }
 
-        private void TrmYears_TextChanged(object sender, EventArgs e)
+        private void TrmYears_TextChanged(object sender, EventArgs e) //input validation for Loan term 
         {
             int val;
             if ((!Int32.TryParse(loanYears.Text, out val)))
@@ -61,7 +61,7 @@ namespace FormAppHW0
             }
         }
 
-        private void IRate_TextChanged(object sender, EventArgs e)
+        private void IRate_TextChanged(object sender, EventArgs e) //input validation for Interest Rate
         {
             double val;
             if ((!double.TryParse(intRate.Text, out val)))
@@ -82,31 +82,33 @@ namespace FormAppHW0
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            if (label4.Visible == false && label5.Visible == false && label6.Visible == false)
+            if (label4.Visible == false && label5.Visible == false && label6.Visible == false) //makes sure input is in correct type and format
             {
-                double lAmount = Convert.ToDouble(loanAmount.Text);
-                double iRate = Convert.ToDouble(intRate.Text) / 100;
-                double loanMonths = Convert.ToDouble(loanYears.Text) * 12;
-                double payMonth = (lAmount) * (Math.Pow((1 + iRate / 12), loanMonths) * iRate) / (12 * (Math.Pow((1 + iRate / 12), loanMonths) - 1));
+                double lAmount = Convert.ToDouble(loanAmount.Text); //changes type of loan amount to double
+                double iRate = Convert.ToDouble(intRate.Text) / 100; // changes Interest rate to double
+                double loanMonths = Convert.ToDouble(loanYears.Text) * 12; // gets interest rate by month and converts it to a double
+                double payMonth = (lAmount) * (Math.Pow((1 + iRate / 12), loanMonths) * iRate) / (12 * (Math.Pow((1 + iRate / 12), loanMonths) - 1)); //monththly payment formula that returns the payment based on the users input for years
 
-                double monthPaid = Math.Round(payMonth, 1);
+                double monthPaid = Math.Round(payMonth, 2); //puts monthly payment into number.XX
 
 
 
-                for (int t =1;t<=loanMonths;t++)
+                for (int t =1;t<=loanMonths;t++) //calculator for loan repayment
                 {
                     
-                    double intpaid = Math.Round(lAmount * (iRate / 12), 3);
-                    double balance = Math.Round((lAmount - monthPaid),3);
-                    double princpaid = Math.Round(monthPaid - intpaid, 3);
+                    double intpaid = Math.Round(lAmount * (iRate / 12), 2); //gets interest paid per month on loan
+                    
+                    double princpaid = Math.Round(monthPaid - intpaid, 2); // gets principal paid per month on loan
 
-                    Solution.AppendText(string.Format("Month: {0}", t));
-                    Solution.AppendText(string.Format("Balance: {0}", balance));
-                    Solution.AppendText(string.Format("Interest Paid: {0}", intpaid));
-                    Solution.AppendText(string.Format("Principal: {0}\t\n", princpaid));
+                    double balance = Math.Round((lAmount - princpaid), 2); // gets remaing balance on the loan after the principal paid each month is is subtracted.
+
+                    Solution.AppendText(string.Format("Month: {0,-10}", t)) ; // adds Month to richtextbox
+                    Solution.AppendText(string.Format("Balance: {0,-20}", balance)); //adds Balace to richtextbox
+                    Solution.AppendText(string.Format("Interest Paid: {0,-20}", intpaid)); // adds Interst paid to richtextbox 
+                    Solution.AppendText(string.Format("Principal: {0,-15}" + "\r\n", princpaid)); // adds Principal paid to richtextbox
                     
 
-                    lAmount = balance;
+                    lAmount = balance; //recomputes new loan Amount
                 }
                
                 
